fb9efc8f5226ac3fb77990ad880f06d9be42fd12
U-C-S <ucs3721@outlook.com>
2021-02-16 15:44:18 +0530

    test

--- Usic.gui/MainWindow.xaml.cs
+++ Usic.gui/MainWindow.xaml.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -11,7 +10,6 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Usic.gui
 {
